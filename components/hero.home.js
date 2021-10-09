9ac4cce3167b48a8e227e22b2a59d760446f6876
Chan Rose <chanrose@pm.me>
2021-10-09 23:45:30 +0700

    add border

--- components/hero.home.js
+++ components/hero.home.js
@@ -2,7 +2,7 @@
 
 export default function HeroHome() {
   return (
-    <section className="flex items-center justify-center bg-rose-200 h-96 parallax-1 w-100">
+    <section className="flex items-center justify-center bg-rose-200 h-96 parallax-1 w-100 border-solid border-t-4 border-b-4 border-rose-400">
             <div className="text-center">
                 <p className="text-xl font-medium tracking-wider text-black">Health Educator</p>
                 <h2 className="mt-3 text-3xl font-bold md:text-5xl text-rose-200">Azla Jonuling </h2>
