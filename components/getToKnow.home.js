bd923002f82582541a9c375bd8300411955a5860
Chan Rose <chanrose@pm.me>
2021-10-09 19:54:50 +0700

    update get to know

--- components/getToKnow.home.js
+++ components/getToKnow.home.js
@@ -1,6 +1,6 @@
 export default function GetToKnowHome() {
   return (
-<div className="min-w-screen min-h-screen bg-blue-100 flex items-center p-5 lg:p-20 overflow-hidden relative">
+<div className="min-w-screen min-h-screen bg-cgray-100 flex items-center p-5 lg:p-20 overflow-hidden relative">
     <div className="flex-1 min-h-full min-w-full rounded-3xl bg-white shadow-xl p-10 lg:p-20 text-gray-800 relative md:flex items-center text-center md:text-left">
         <div className="w-full md:w-1/2">
             <div className="mb-10 lg:mb-20">
@@ -21,8 +21,8 @@ export default function GetToKnowHome() {
         
         </div>
     </div>
-    <div className="w-64 md:w-96 h-96 md:h-full bg-blue-200 bg-opacity-30 absolute -top-64 md:-top-96 right-20 md:right-32 rounded-full pointer-events-none -rotate-45 transform"></div>
-    <div className="w-96 h-full bg-indigo-200 bg-opacity-20 absolute -bottom-96 right-64 rounded-full pointer-events-none -rotate-45 transform"></div>
+    <div className="w-64 md:w-96 h-96 md:h-full bg-rose-200 bg-opacity-30 absolute -top-64 md:-top-96 right-20 md:right-32 rounded-full pointer-events-none -rotate-45 transform"></div>
+    <div className="w-96 h-full bg-rose-200 bg-opacity-20 absolute -bottom-96 right-64 rounded-full pointer-events-none -rotate-45 transform"></div>
 </div>
   );
 }
