a44f6b32adf0ef59efa02ad19721e1130e030a3f
Chan Rose <chanrose@pm.me>
2021-10-10 17:56:33 +0700

    update recent

---
+++ components/card.home.js
@@ -0,0 +1,22 @@
+export default function Card({props}) {
+  return (
+            <div className="overflow-hidden shadow-lg rounded-lg h-90 w-60 md:w-80 cursor-pointer m-auto">
+            <a href={props.a} className="w-full block h-full">
+              <img
+                alt="blog photo"
+                src={props.img}
+                className="max-h-40 w-full object-cover"
+              />
+              <div className="bg-white dark:bg-rose-200 w-full p-4">
+                <p className="text-rose-300 dark:text-gray-600 text-md font-medium">{props.category}</p>
+                <p className="text-cgray-900 text-xl font-medium mb-2">
+                  {props.title}
+                </p>
+                <p className="text-gray-400 dark:text-cgray-600 font-light text-md">
+                  {props.description}
+                </p>
+              </div>
+            </a>
+          </div>
+  );
+}
--- components/recent.home.js
+++ components/recent.home.js
@@ -1,48 +1,36 @@
+import Card from "./card.home";
+
 export default function RecentHome() {
+  const recent = [
+    {
+      category: "Blog",
+      title: "MOMO MY BEST FRIEND",
+      description: "This is my late Bunny. click to read the full story.",
+      a: "#",
+      img: "/assets/home/momo_frame.png",
+    },
+    {
+      category: "Vlog",
+      title: "VLOG: DORM ROOM TOUR",
+      description:
+        "Curious how to make your dorm room to look aesthetic? Check mine!",
+      a: "#",
+      img: "/assets/home/vlog_room.png",
+    },
+  ];
+
   return (
     <section className="p-6">
       <h2 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-cgray-500 dark:text-cgray-50 text-center">
         {" "}
         Recent{" "}
       </h2>
-      <div className="flex flex-row gap justify-center content-center">
-           <div className="overflow-hidden shadow-lg rounded-lg h-90 w-60 md:w-80 cursor-pointer m-auto">
-          <a href="#" className="w-full block h-full">
-            <img
-              alt="blog photo"
-              src="/assets/home/momo_frame.png"
-              className="max-h-40 w-full object-cover"
-            />
-            <div className="bg-white dark:bg-rose-200 w-full p-4">
-              <p className="text-rose-400 text-md font-medium">Blog</p>
-              <h5 className="text-gray-800 dark:text-cgray-600 text-xl font-medium mb-2">
-                MOMO MY BEST FRIEND
-              </h5>
-              <p className="text-gray-400 dark:text-cgray-500 font-light text-md">
-                This is my late Bunny. click to read the full story.
-              </p>
-            </div>
-          </a>
-        </div>
 
-
-        <div className="overflow-hidden shadow-lg rounded-lg h-90 w-60 md:w-80 cursor-pointer m-auto">
-          <a href="#" className="w-full block h-full">
-            <img
-              alt="blog photo"
-              src="/assets/home/momo_frame.png"
-              className="max-h-40 w-full object-cover"
-            />
-            <div className="bg-white dark:bg-rose-200 w-full p-4">
-              <p className="text-rose-400 text-md font-medium">Blog</p>
-              <h5 className="text-gray-800 dark:text-cgray-600 text-xl font-medium mb-2">
-                MOMO MY BEST FRIEND
-              </h5>
-              <p className="text-gray-400 dark:text-cgray-500 font-light text-md">
-                This is my late Bunny. click to read the full story.
-              </p>
-            </div>
-          </a>
+      <div className="w-full p-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-12">
+          {recent.map((item) => (
+            <Card props={item} />
+          ))}
         </div>
       </div>
     </section>
