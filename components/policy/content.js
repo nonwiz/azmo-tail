64c54d3f748bd89ff301afea6022f35381a17585
Chan Rose <chanrose@pm.me>
2021-10-11 21:27:57 +0700

    update privacy

--- components/policy/content.js
+++ components/policy/content.js
@@ -31,7 +31,6 @@ export default function ContentPrivacy() {
               telephone number.
             </p>
           </div>
-
           <div className="my-8">
             <h2 className="text-3xl font-bold text-wgray-600 dark:text-wgray-500">
               How we use your information
@@ -56,6 +55,109 @@ export default function ContentPrivacy() {
               <li>Send you emails</li>
               <li>Find and prevent fraud</li>
             </ul>
+          </div>
+
+          <div className="my-8">
+            <h2 className="text-3xl font-bold text-wgray-600 dark:text-wgray-500">
+              CCPA Privacy Rights (Do Not Sell My Personal Information)
+            </h2>
+
+            <p className="mt-4">
+              Under the CCPA, among other rights, California consumers have the
+              right to:
+            </p>
+            <ul className="text-base list-disc ml-3 mt-3">
+              <li>
+                Request that a business that collects a consumer's personal data
+                disclose the categories and specific pieces of personal data
+                that a business has collected about consumers.
+              </li>
+              <li>
+                Request that a business delete any personal data about the
+                consumer that a business has collected.
+              </li>
+              <li>
+                Request that a business that sells a consumer's personal data,
+                not sell the consumer's personal data.
+              </li>
+              <li>
+                If you make a request, we have one month to respond to you. If
+                you would like to exercise any of these rights, please contact
+                us.
+              </li>
+            </ul>
+          </div>
+
+          <div className="my-8">
+            <h2 className="text-3xl font-bold text-wgray-600 dark:text-wgray-500">
+              GDPR Data Protection Rights
+            </h2>
+
+            <p className="mt-4">
+              We would like to make sure you are fully aware of all of your data
+              protection rights. Every user is entitled to the following:
+            </p>
+            <ul className="text-base list-disc ml-3 my-3">
+              <li>
+                {" "}
+                The right to access – You have the right to request copies of
+                your personal data. We may charge you a small fee for this
+                service.{" "}
+              </li>
+              <li>
+                {" "}
+                The right to rectification – You have the right to request that
+                we correct any information you believe is inaccurate. You also
+                have the right to request that we complete the information you
+                believe is incomplete.{" "}
+              </li>
+              <li>
+                The right to erasure – You have the right to request that we
+                erase your personal data, under certain conditions.
+              </li>
+              <li>
+                {" "}
+                The right to restrict processing – You have the right to request
+                that we restrict the processing of your personal data, under
+                certain conditions.{" "}
+              </li>
+              <li>
+                {" "}
+                The right to object to processing – You have the right to object
+                to our processing of your personal data, under certain
+                conditions.{" "}
+              </li>
+              <li>
+                {" "}
+                The right to data portability – You have the right to request
+                that we transfer the data that we have collected to another
+                organization, or directly to you, under certain conditions.{" "}
+              </li>
+            </ul>
+            <p>
+              {" "}
+              If you make a request, we have one month to respond to you. If you
+              would like to exercise any of these rights, please contact us.{" "}
+            </p>
+          </div>
+          <div className="my-8">
+            <h2 className="text-3xl font-bold text-wgray-600 dark:text-wgray-500">
+              Children's Information
+            </h2>
+
+            <p className="mt-4">
+              Another part of our priority is adding protection for children
+              while using the internet. We encourage parents and guardians to
+              observe, participate in, and/or monitor and guide their online
+              activity.
+              <br />
+              Azmo does not knowingly collect any Personal Identifiable
+              Information from children under the age of 13. If you think that
+              your child provided this kind of information on our website, we
+              strongly encourage you to contact us immediately and we will do
+              our best efforts to promptly remove such information from our
+              records.
+            </p>
           </div>
         </div>
       </section>
