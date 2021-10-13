eb5568f2e75f1d1713adce4fb56dd7dbbaf20299
Chan Rose <chanrose@pm.me>
2021-10-13 09:13:28 +0700

    update policy privacy hero section

--- pages/privacy.js
+++ pages/privacy.js
@@ -5,16 +5,20 @@ import SliderHome from "../components/slider.home";
 import QuoteHome from "../components/quote.home";
 import GetToKnowHome from "../components/getToKnow.home";
 import Image from "next/image";
-import ContentPrivacy from '../components/policy/content.js';
-import SlideShow from '../components/hero.slideshow.js';
+import ContentPrivacy from '../components/policy/content';
+import SlideShow from '../components/hero.slideshow';
+import HeroPostLayout from '../components/hero.layout';
 
 export default function Privacy() {
+  const item = {'title': 'Privacy Policy', 'content': <> <a href="/"> Azmo </a> / Policy Privacy </>, img: '', alt: ''}
+
+
   return (
     <Layout>
       <Head>
         <title>Privacy Policy | Azmo</title>
       </Head>
-      <SlideShow /> 
+      <HeroPostLayout item={item} />
         <div className="mt-14"></div>
       <QuoteHome />
             <div className="mt-5"></div>
