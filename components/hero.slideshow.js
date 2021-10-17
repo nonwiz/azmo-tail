aee7864e7729260505e327a2075857d7eeda41d2
Chan Rose <chanrose@pm.me>
2021-10-17 22:18:53 +0700

    update layout

--- components/hero.slideshow.js
+++ components/hero.slideshow.js
@@ -9,7 +9,12 @@ const images = [
   "/assets/slider/fruit.png",
 ];
 
-const content = "Lifestyle, Vlogging, & Blogging, and Tips about Health";
+const content = (
+  <span>
+    Lifestyle, Vlogging, & Blogging,
+    <br /> and Tips about Health
+  </span>
+);
 
 const slides = [
   { title: "AZMO CHERISH", content: content, img: images[0], alt: "" },
@@ -28,8 +33,8 @@ export default function SlideShow() {
               style={{ backgroundImage: `url(${item.img})` }}
               className="bg-cover bg-center  h-auto text-white py-24 md:py-32 lg:py-44 px-10 object-fill"
             >
-              <div className="md:w-1/2 mx-auto text-center sm:h-40 flex flex-col justify-center bg-white bg-opacity-30 md:bg-opacity-10 rounded-xl px-2">
-                <h1 className="text-4xl font-bold font-extrabold gagalin text-name text-name">
+              <div className="md:w-1/2 mx-auto text-center sm:h-40 flex flex-col justify-center bg-white bg-opacity-50 md:bg-opacity-50 rounded-xl px-2">
+                <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold font-extrabold gagalin text-name text-name">
                   <span className="text-name">{item.title}</span>
                 </h1>
                 <hr className="mt-4 border-2 mx-auto w-4/5 border-wgray-400" />
